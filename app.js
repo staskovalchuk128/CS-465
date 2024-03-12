7e6d9122bd3eeca35e565e70da2de51903c0ff4f
staskovalchuk128 <staskovalchuk128@gmail.com>
2024-03-12 12:26:17 -0500

    Module 2 completed baseline

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
+
+var handebars = require('hbs');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+handebars.registerPartials(__dirname + '/app_server/views/partials');
 app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/travel', travelRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
